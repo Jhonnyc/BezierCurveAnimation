16a898180001ae9e67b9c4a98abee95338e0cdce
Yoni Kalangan <yoni.kalangan@gmail.com>
2017-10-22 15:30:08 +0300

    * Auto indent and auto remove of unused imports

--- uibezierpath/src/main/java/com/yonikal/uibezierpath/UIBezierPath.java
+++ uibezierpath/src/main/java/com/yonikal/uibezierpath/UIBezierPath.java
@@ -10,7 +10,6 @@ import android.os.Handler;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
