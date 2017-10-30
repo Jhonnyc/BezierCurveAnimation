2ae0aed587dcd4b8f7bd67c694cb6960ee57525c
Yoni Kalangan <yoni.kalangan@gmail.com>
2017-10-30 07:51:20 +0200

    * Added the ui bezier class to the test activity for checkups

--- app/src/main/java/com/yonikal/beziercurveanimation/ActivityTest.java
+++ app/src/main/java/com/yonikal/beziercurveanimation/ActivityTest.java
@@ -3,7 +3,10 @@ package com.yonikal.beziercurveanimation;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+
+import com.yonikal.uibezierpath.UIBezierPath;
 
 /**
  * Created by yonikal on 19/08/17.
@@ -11,12 +14,16 @@ import android.widget.RelativeLayout;
 public class ActivityTest extends Activity {
 
     private RelativeLayout mContainer;
+    private ImageView mPlaneImageView;
+    private UIBezierPath mBezierPath;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
         mContainer = (RelativeLayout) findViewById(R.id.container);
+        mPlaneImageView = (ImageView) findViewById(R.id.plane_iv);
+        mBezierPath = new UIBezierPath(mContainer, mPlaneImageView, R.mipmap.plane);
     }
 
     @Override
