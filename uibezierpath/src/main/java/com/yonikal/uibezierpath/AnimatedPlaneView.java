c85e05dc0c589f8aa3c92672aafc3be300a3744e
Yoni Kalangan <yoni.kalangan@gmail.com>
2017-11-14 10:08:55 +0200

    * Added a class to wrap the animated plane

---
+++ uibezierpath/src/main/java/com/yonikal/uibezierpath/AnimatedPlaneView.java
@@ -0,0 +1,91 @@
+package com.yonikal.uibezierpath;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
+import android.graphics.Color;
+import android.graphics.PathMeasure;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import java.util.Random;
+
+/**
+ * Created by yoni on 14/11/2017.
+ */
+
+public class AnimatedPlaneView {
+
+    private static final int MIN_ANIMATION_TIME = 1800;
+    private static final int MAX_ANIMATION_TIME = 3000;
+    private static final int[] COLORS = new int[]{Color.RED, Color.YELLOW,
+            Color.GREEN, Color.BLUE, Color.MAGENTA, Color.CYAN};
+
+    float[] last;
+    ImageView imageView;
+    ValueAnimator pathAnimator;
+
+    public AnimatedPlaneView(final ViewGroup parentView, DataPath path, int imageview) {
+        last = new float[]{parentView.getX() + parentView.getHeight() / 2, parentView.getWidth() / 2};
+        imageView = new ImageView(parentView.getContext());
+        imageView.setImageResource(imageview);
+
+        RelativeLayout.LayoutParams rlParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,
+                RelativeLayout.LayoutParams.WRAP_CONTENT);
+        imageView.setLayoutParams(rlParams);
+        imageView.setX(path.getStartX());
+        imageView.setY(path.getStartY());
+        int color = COLORS[new Random().nextInt(COLORS.length)];
+        imageView.setColorFilter(color);
+
+        parentView.addView(imageView);
+
+        final PathMeasure pm = new PathMeasure(path, false);
+
+        pathAnimator = ValueAnimator.ofFloat(0.0f, pm.getLength());
+        pathAnimator.setDuration(new Random().nextInt(MAX_ANIMATION_TIME - MIN_ANIMATION_TIME) + MIN_ANIMATION_TIME);
+        pathAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                float val = animation.getAnimatedFraction();
+
+                float distance = pm.getLength();
+                if (val != 0) {
+                    distance = pm.getLength() * val;
+                }
+                float[] tan = new float[2];
+                imageView.setX(last[0]);
+                imageView.setY(last[1]);
+
+                pm.getPosTan(distance, last, tan);
+                float deg = (float) (Math.atan2(tan[1], tan[0]) * 180.0 / Math.PI);
+                imageView.setRotation(deg);
+            }
+        });
+        pathAnimator.addListener(new AnimatorListenerAdapter() {
+
+            @Override
+            public void onAnimationStart(Animator animation) {
+                super.onAnimationStart(animation);
+                imageView.setLayerType(View.LAYER_TYPE_HARDWARE, null);
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                imageView.setVisibility(View.GONE);
+                imageView.setLayerType(View.LAYER_TYPE_NONE, null);
+                imageView.setDrawingCacheEnabled(false);
+                parentView.removeView(imageView);
+            }
+
+        });
+    }
+
+    public void startAnimation() {
+        pathAnimator.start();
+    }
+
+}
